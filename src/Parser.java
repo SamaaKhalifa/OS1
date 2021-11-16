fdb684288d33d89f39a98c82a6fa67bd1df65fe6
SamaaKhalifa <83132190+SamaaKhalifa@users.noreply.github.com>
2021-11-16 19:36:57 +0200

    Update Parser.java

--- src/Parser.java
+++ src/Parser.java
@@ -5,17 +5,44 @@ class Parser {
     //This method will divide the input into commandName and args
 //where "input" is the string command entered by the user
     public boolean parse(String input){};
-    public String getCommandName(){};
-    public String[] getArgs(){};
+    public String getCommandName(){
+            return commandName;
+    };
+    public String[] getArgs(){
+            return  args;
+    };
 }
 public class Terminal {
     Parser parser;
+    String HomePath=System.getProperty("user.home") + "/Desktop";
+    String CurrentPath;
     //Implement each command in a method, for example:
-    public String pwd(){...}
-    public void cd(String[] args){...}
-    // ...
+    public void echo(String arg){
+        System.out.println(arg+"\n");
+    }
+    public void cd(String[] arg){
+        if(arg.length()==0){
+                CurrentPath=HomePath;
+        }else{
+            if(arg[0]==".."){
+                int x= CurrentPath.lastIndexOf('/');
+                CurrentPath=CurrentPath.substring(0,x);
+            }else{
+                CurrentPath=arg[0];
+            }
+        }
+    }
 //This method will choose the suitable command method to be called
-    public void chooseCommandAction(){...}
-    public static void main(String[] args){...}
+    public void chooseCommandAction(){
+
+        String CommName=parser.getCommandName();
+        String[] arg= parser.getArgs();
+        if(CommName=="echo"){
+            echo(arg[1]);
+        }else if(CommName=="cd"){
+            cd(arg);
+        }
+    };
+    public static void main(String[] args){...};
 }
 
