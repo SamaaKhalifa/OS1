0f9d1f1378bde1ef870a5135880a51a07fa6f857
SamaaKhalifa <83132190+SamaaKhalifa@users.noreply.github.com>
2021-11-17 15:22:13 +0200

    Update Parser.java

--- src/Parser.java
+++ src/Parser.java
@@ -1,6 +1,7 @@
 import javax.lang.model.type.NullType;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Paths;
 import java.util.Arrays;
@@ -9,14 +10,21 @@ import java.util.Scanner;
 
 class Parser {
     String commandName;
-    String[] args;
+    String[] args=new String[]{};
 
     //This method will divide the input into commandName and args
 //where "input" is the string command entered by the user
     public void parse(String input){
+        if (input.length()>0){
         int x= input.indexOf(" ");
-        commandName=input.substring(0,x);
-        args=input.substring(x).split(" ");
+        if(x>=0){
+            commandName=input.substring(0,x);
+            args=input.substring(x).split(" ");
+        }
+        else{
+           commandName=input;
+           args=null;
+        } }
     };
     public String getCommandName(){
             return commandName;
@@ -26,10 +34,11 @@ class Parser {
     };
 }
 class Terminal {
-    private static Parser parser;
-   // Parser parser;
-    String HomePath=System.getProperty("user.home") + "/Desktop";
-    String CurrentPath;
+    //private static Parser parser;
+    Parser parser=new Parser();
+    String HomePath=System.getProperty("user.home")+"\\OS";
+    File h=new File(HomePath);
+    String CurrentPath=HomePath;
     public static boolean isPathValid(String path) {
         try {
 
@@ -43,10 +52,10 @@ class Terminal {
     }
     //Implement each command in a method, for example:
     public void echo(String arg){
-        System.out.println(arg+"\n");
+        System.out.println(arg);
     }
     public void cd(String[] arg){
-        if(arg[0]== null){
+        if(arg==null){
                 CurrentPath=HomePath;
         }else{
             if(arg[0]==".."){
@@ -57,18 +66,14 @@ class Terminal {
                 else System.out.println("invalid Path\n");
         }
         }
+        System.out.println(CurrentPath);
     }
     public void  ls()  {
-        File f = new File(CurrentPath); // current directory
-
-        File[] files = f.listFiles();
-        Arrays.sort(files);
-        for (File file : files) {
-            try {
-                System.out.println(file.getCanonicalPath());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        File dir=new File(CurrentPath);
+        File dir1 = new File(System.setProperty("user.dir",dir.getAbsolutePath()));
+        String childs[] = dir.list();
+        for(String child: childs){
+            System.out.println(child);
         }
     }
     public void lsR(){
@@ -79,34 +84,44 @@ class Terminal {
 
         for (File file : files) {
             try {
-                System.out.println(file.getCanonicalPath());
+                int x=file.getCanonicalPath().lastIndexOf("\\");
+                System.out.println(file.getCanonicalPath().substring(x+1));
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
-
 //This method will choose the suitable command method to be called
     public void chooseCommandAction(){
-
+        if(h.isDirectory()){
+            System.setProperty("user.dir",h.getAbsolutePath());
+        }
         String CommName=parser.getCommandName();
         String[] argument= parser.getArgs();
-        if(CommName=="echo"){
-            echo(argument[1]);
-        }else if(CommName=="cd"){
+        //System.out.println(CommName+" Hello");
+        if(CommName.contains("echo")){
+            this.echo(argument[1]);
+        }else if(CommName.contains("cd")){
             cd(argument);
-        }else if(CommName=="ls"){
-                ls();
-
-        }else if(CommName=="ls -r"){
-                lsR();
+        }else if(CommName.contains("ls")&!CommName.contains("ls-r")){
+               this.ls();
+        }else if(CommName.contains("ls-r")){
+                this.lsR();
         }
     };
     public static void main(String[] args){
         String input;
         Scanner sc=new Scanner(System.in);
-        input=sc.nextLine();
-        parser.parse(input);
+        System.out.println("Enter the input:");
+        while(true){
+            input=sc.nextLine();
+            if(input.contains("exit")){
+                break;
+            }
+            Terminal t=new Terminal();
+            t.parser.parse(input);
+            t.chooseCommandAction();
+        }
 
     };
 }
